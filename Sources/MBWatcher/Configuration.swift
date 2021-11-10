0140ffcf610015f0b415bc86a8971c861deebc27
Dmitriy Bogomolov <Dimones951@yandex.ru>
2021-11-10 08:17:26 +0700

    Изменил буки для поиска

--- Sources/MBWatcher/Configuration.swift
+++ Sources/MBWatcher/Configuration.swift
@@ -11,5 +11,5 @@ class Configuration
 {
 	static let fetchInterval: TimeInterval = 60 * 5
 	static let botSleep: TimeInterval = 10
-	static let booksToFind: [MBResult] = [.mainBook(), .testBook(), .topBook()]
+	static let booksToFind: [MBResult] = [.mainBook(), .topBook()]
 }
